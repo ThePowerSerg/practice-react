60f3570218a6f87af9df01e02e6fcd98f61aed6b
ThePowerSerg <sferreir@bu.edu>
2026-06-18 10:33:39 -0400

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import { Header } from "./Header";
 
 function App() {
     return (
-        <h1>Practice React</h1>
+        <>
+            <Header/>
+        </>
     );
 }
 export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Serg's Kitchen</h1>
+        </header>
+    )
+}
