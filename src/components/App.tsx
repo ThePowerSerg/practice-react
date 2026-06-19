9b7b78508149cb147d9bae0b617d4732d5848325
ThePowerSerg <sferreir@bu.edu>
2026-06-19 12:21:29 -0400

    add hooks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,11 @@
 import "../App.css";
 import { Header } from "../components/Header";
-import type { Person } from "../models/person";
 import { DisplaySports } from "../components/DisplaySports";
-import type { Sports } from "../models/sports";
-
-const person: Person = {name: "Mario", age: 10, dob: new Date("1/1/2016")}
-
-const sports: Sports[] = [
-    {id: 1, name: "Swimming"},
-    {id: 2, name: "Biking"},
-    {id: 3, name: "Running"},
-]
+import { useAppData } from "../hooks/useAppData";
 
 export default function App() {
+    const { person, sports } = useAppData()
+
     return (
         <>
             <Header {...person} />
