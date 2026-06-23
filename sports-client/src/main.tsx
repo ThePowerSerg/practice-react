839e984ad1e46aaf6ae1543569243f06064bca07
ThePowerSerg <sferreir@bu.edu>
2026-06-23 09:07:10 -0400

    add biker image and tests

--- sports-client/src/main.tsx
+++ sports-client/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.tsx'
 
+// array destructuring example
+const [, zebra] = ["giraffe", "zebra", "bear"]
+console.log(zebra)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
